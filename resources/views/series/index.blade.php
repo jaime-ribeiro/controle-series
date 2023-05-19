78ba5a805080c3c3bc34ed413fc10aebe02f168a
jaime-ribeiro <littlejou@gmail.com>
2023-05-18 23:32:29 -0300

    Usando javascript e convertendo o valor da variavel php pra dentro da variávle javascript

--- resources/views/series/index.blade.php
+++ resources/views/series/index.blade.php
@@ -7,4 +7,8 @@
 
     {{-- o blade vai ignorar o {{nome}} e não vai considerar ele como variável se utilizar o @ na frente --}}
     @{{nome}}
+
+    <script>
+        const series = {{Js::from($series)}};
+    </script>
 </x-layout>
